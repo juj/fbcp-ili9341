2eb44fb717ebbd2d6908eded060facd864fd6df6
Jukka Jylänki <jujjyl@gmail.com>
2018-04-26 18:03:56 +0000

    Default to capturing source content at 120fps.

--- display.h
+++ display.h
@@ -1,7 +1,7 @@
 #pragma once
 
 // Configures the desired display update rate. Use 120 for max performance/minimized latency, and 60/50/30/24 etc. for regular content, or to save battery.
-#define TARGET_FRAME_RATE 60
+#define TARGET_FRAME_RATE 120
 
 #ifdef ILI9341
 #include "ili9341.h"
