2462cceaa2743bb4fd987e0cad83789aa3d7c4de
Jukka Jylänki <jujjyl@gmail.com>
2018-05-16 16:24:36 +0300

    Add GET_GPIO(pin) functionality.

--- spi.h
+++ spi.h
@@ -57,12 +57,14 @@ typedef struct GPIORegisterFile
 {
   uint32_t gpfsel[6], reserved0; // GPIO Function Select registers, 3 bits per pin, 10 pins in an uint32_t
   uint32_t gpset[2], reserved1; // GPIO Pin Output Set registers, write a 1 to bit at index I to set the pin at index I high
-  uint32_t gpclr[2]; // GPIO Pin Output Clear registers, write a 1 to bit at index I to set the pin at index I low
+  uint32_t gpclr[2], reserved2; // GPIO Pin Output Clear registers, write a 1 to bit at index I to set the pin at index I low
+  uint32_t gplev[2];
 } GPIORegisterFile;
 extern volatile GPIORegisterFile *gpio;
 
 #define SET_GPIO_MODE(pin, mode) gpio->gpfsel[(pin)/10] = (gpio->gpfsel[(pin)/10] & ~(0x7 << ((pin) % 10) * 3)) | ((mode) << ((pin) % 10) * 3)
 #define GET_GPIO_MODE(pin) ((gpio->gpfsel[(pin)/10] & (0x7 << ((pin) % 10) * 3)) >> (((pin) % 10) * 3))
+#define GET_GPIO(pin) (gpio->gplev[0] & (1 << (pin))) // Pin must be (0-31)
 #define SET_GPIO(pin) gpio->gpset[0] = 1 << (pin) // Pin must be (0-31)
 #define CLEAR_GPIO(pin) gpio->gpclr[0] = 1 << (pin) // Pin must be (0-31)
 
