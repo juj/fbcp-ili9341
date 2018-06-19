0121e6a0a3fa77045057968c9ffcb413cd36fca5
Jukka Jylänki <jujjyl@gmail.com>
2018-06-19 15:18:59 +0000

    Code cleanup: Remove need to custom define DISPLAY_COVERED_*_SIDE variables twice for both landscape and portrait, but have display.h manage the logic of swapping the directions around if needed.

--- freeplaytech_waveshare32b.h
+++ freeplaytech_waveshare32b.h
@@ -11,20 +11,14 @@
 #define GPIO_TFT_RESET_PIN 27
 #endif
 
-#if defined(DISPLAY_FLIP_ORIENTATION_IN_SOFTWARE)
 // On FreePlayTech GBA devices, a part of the screen is hidden by the bezels, since the GBA has a 2.8" screen surface area, but a 3.2" display is enclosed inside the case.
 // The hidden area is placed under the left edge of the display horizontally, and under top and bottom edges of the display vertically, so reduce those out from the drawable area.
 // Effective display area is then 320-18=302 pixels horizontally, and 202 pixels vertically (in landscape direction).
+
+// These numbers are specified in portrait mode (240x320 direction).
 #define DISPLAY_COVERED_TOP_SIDE 18
 #define DISPLAY_COVERED_LEFT_SIDE 9
 #define DISPLAY_COVERED_RIGHT_SIDE 29
 #define DISPLAY_COVERED_BOTTOM_SIDE 0
-#else
-#define DISPLAY_COVERED_TOP_SIDE 9
-#define DISPLAY_COVERED_LEFT_SIDE 18
-#define DISPLAY_COVERED_RIGHT_SIDE 0
-#define DISPLAY_COVERED_BOTTOM_SIDE 29
-
-#endif
 
 #endif
