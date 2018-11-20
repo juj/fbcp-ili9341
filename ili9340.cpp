3493d29a0aefa7ff96842c13616ee2ecd50e6d65
Joona Heinikoski <joona.heinikoski@reaktor.com>
2018-11-20 22:17:02 +0200

    Rename all 9341 -> 9340

--- ili9340.cpp
+++ ili9340.cpp
@@ -1,13 +1,13 @@
 #include "config.h"
 
-#ifdef ILI9341
+#ifdef ILI9340
 
 #include "spi.h"
 
 #include <memory.h>
 #include <stdio.h>
 
-void InitILI9341()
+void InitILI9340()
 {
   // If a Reset pin is defined, toggle it briefly high->low->high to enable the device. Some devices do not have a reset pin, in which case compile with GPIO_TFT_RESET_PIN left undefined.
 #if defined(GPIO_TFT_RESET_PIN) && GPIO_TFT_RESET_PIN >= 0
@@ -30,16 +30,16 @@ void InitILI9341()
     SPI_TRANSFER(0x01/*Software Reset*/);
     usleep(5*1000);
     SPI_TRANSFER(0x28/*Display OFF*/);
-    // The following appear in ILI9341 Data Sheet v1.11 (2011/06/10), but not in older v1.02 (2010/12/06).
+    // The following appear in ILI9340 Data Sheet v1.11 (2011/06/10), but not in older v1.02 (2010/12/06).
     SPI_TRANSFER(0xCB/*Power Control A*/, 0x39/*Reserved*/, 0x2C/*Reserved*/, 0x00/*Reserved*/, 0x34/*REG_VD=1.6V*/, 0x02/*VBC=5.6V*/); // These are the same as power on.
-    SPI_TRANSFER(0xCF/*Power Control B*/, 0x00/*Always Zero*/, 0xC1/*Power Control=0,DRV_ena=0,PCEQ=1*/, 0x30/*DC_ena=1*/); // Not sure what the effect is, set to default as per ILI9341 Application Notes v0.6 (2011/03/11) document (which is not apparently same as default at power on).
-    SPI_TRANSFER(0xE8/*Driver Timing Control A*/, 0x85, 0x00, 0x78); // Not sure what the effect is, set to default as per ILI9341 Application Notes v0.6 (2011/03/11) document (which is not apparently same as default at power on).
-    SPI_TRANSFER(0xEA/*Driver Timing Control B*/, 0x00, 0x00); // Not sure what the effect is, set to default as per ILI9341 Application Notes v0.6 (2011/03/11) document (which is not apparently same as default at power on).
-    SPI_TRANSFER(0xED/*Power On Sequence Control*/, 0x64, 0x03, 0x12, 0x81); // Not sure what the effect is, set to default as per ILI9341 Application Notes v0.6 (2011/03/11) document (which is not apparently same as default at power on).
-#if ILI9341_UPDATE_FRAMERATE == ILI9341_FRAMERATE_119_HZ // Setting pump ratio if update rate is less than 119 Hz does not look good but produces shimmering in panning motion.
-    SPI_TRANSFER(0xF7/*Pump Ratio Control*/, 0x30/*DDVDH=3xVCI*/); // Appears in ILI9341 Data Sheet v1.11 (2011/06/10), but not in older v1.02 (2010/12/06). This has a subtle effect on colors/saturation. Valid values are 0x20 and 0x30. Spec says 0x20 is default at boot, but doesn't seem so, more like 0x00 is default, giving supersaturated colors. I like 0x30 best.
+    SPI_TRANSFER(0xCF/*Power Control B*/, 0x00/*Always Zero*/, 0xC1/*Power Control=0,DRV_ena=0,PCEQ=1*/, 0x30/*DC_ena=1*/); // Not sure what the effect is, set to default as per ILI9340 Application Notes v0.6 (2011/03/11) document (which is not apparently same as default at power on).
+    SPI_TRANSFER(0xE8/*Driver Timing Control A*/, 0x85, 0x00, 0x78); // Not sure what the effect is, set to default as per ILI9340 Application Notes v0.6 (2011/03/11) document (which is not apparently same as default at power on).
+    SPI_TRANSFER(0xEA/*Driver Timing Control B*/, 0x00, 0x00); // Not sure what the effect is, set to default as per ILI9340 Application Notes v0.6 (2011/03/11) document (which is not apparently same as default at power on).
+    SPI_TRANSFER(0xED/*Power On Sequence Control*/, 0x64, 0x03, 0x12, 0x81); // Not sure what the effect is, set to default as per ILI9340 Application Notes v0.6 (2011/03/11) document (which is not apparently same as default at power on).
+#if ILI9340_UPDATE_FRAMERATE == ILI9340_FRAMERATE_119_HZ // Setting pump ratio if update rate is less than 119 Hz does not look good but produces shimmering in panning motion.
+    SPI_TRANSFER(0xF7/*Pump Ratio Control*/, 0x30/*DDVDH=3xVCI*/); // Appears in ILI9340 Data Sheet v1.11 (2011/06/10), but not in older v1.02 (2010/12/06). This has a subtle effect on colors/saturation. Valid values are 0x20 and 0x30. Spec says 0x20 is default at boot, but doesn't seem so, more like 0x00 is default, giving supersaturated colors. I like 0x30 best.
 #endif
-    // The following appear also in old ILI9341 Data Sheet v1.02 (2010/12/06).
+    // The following appear also in old ILI9340 Data Sheet v1.02 (2010/12/06).
     SPI_TRANSFER(0xC0/*Power Control 1*/, 0x23/*VRH=4.60V*/); // Set the GVDD level, which is a reference level for the VCOM level and the grayscale voltage level.
     SPI_TRANSFER(0xC1/*Power Control 2*/, 0x10/*AVCC=VCIx2,VGH=VCIx7,VGL=-VCIx4*/); // Sets the factor used in the step-up circuits. To reduce power consumption, set a smaller factor.
     SPI_TRANSFER(0xC5/*VCOM Control 1*/, 0x3e/*VCOMH=4.250V*/, 0x28/*VCOML=-1.500V*/); // Adjusting VCOM 1 and 2 can control display brightness
@@ -86,10 +86,10 @@ void InitILI9341()
 
     // It seems that in internal clock mode, horizontal front and back porch settings (HFP, BFP) are ignored(?)
 
-    SPI_TRANSFER(0xB1/*Frame Rate Control (In Normal Mode/Full Colors)*/, 0x00/*DIVA=fosc*/, ILI9341_UPDATE_FRAMERATE/*RTNA(Frame Rate)*/);
+    SPI_TRANSFER(0xB1/*Frame Rate Control (In Normal Mode/Full Colors)*/, 0x00/*DIVA=fosc*/, ILI9340_UPDATE_FRAMERATE/*RTNA(Frame Rate)*/);
 //    SPI_TRANSFER(0xB5/*Blanking Porch Control*/, 0x02/*VFP, vertical front porch*/, 0x02/*VBP, vertical back porch*/, 0x0A/*HFP, horizontal front porch*/, 0x14/*HBP, horizontal back porch*/); // These are the default values at power on
     SPI_TRANSFER(0xB6/*Display Function Control*/, 0x08/*PTG=Interval Scan,PT=V63/V0/VCOML/VCOMH*/, 0x82/*REV=1(Normally white),ISC(Scan Cycle)=5 frames*/, 0x27/*LCD Driver Lines=320*/);
-    SPI_TRANSFER(0xF2/*Enable 3G*/, 0x02/*False*/); // This one is present only in ILI9341 Data Sheet v1.11 (2011/06/10)
+    SPI_TRANSFER(0xF2/*Enable 3G*/, 0x02/*False*/); // This one is present only in ILI9340 Data Sheet v1.11 (2011/06/10)
     SPI_TRANSFER(0x26/*Gamma Set*/, 0x01/*Gamma curve 1 (G2.2)*/);
     SPI_TRANSFER(0xE0/*Positive Gamma Correction*/, 0x0F, 0x31, 0x2B, 0x0C, 0x0E, 0x08, 0x4E, 0xF1, 0x37, 0x07, 0x10, 0x03, 0x0E, 0x09, 0x00);
     SPI_TRANSFER(0xE1/*Negative Gamma Correction*/, 0x00, 0x0E, 0x14, 0x03, 0x11, 0x07, 0x31, 0xC1, 0x48, 0x08, 0x0F, 0x0C, 0x31, 0x36, 0x0F);
--- ili9340.h
+++ ili9340.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#ifdef ILI9341
+#ifdef ILI9340
 
 // SPI_BUS_CLOCK_DIVISOR specifies how fast to communicate the SPI bus at. Possible values are 4, 6, 8, 10, 12, ... Smaller
 // values are faster. On my PiTFT 2.8 and Waveshare32b displays, divisor value of 4 does not work, and
@@ -23,47 +23,47 @@
 // core_freq=338: CDIV=4, results in 84.5MHz, works
 // core_freq=339: CDIV=4, would result in 84.75MHz, would work most of the time, but every few minutes generated random glitched pixels.
 
-// Data specific to the ILI9341 controller
+// Data specific to the ILI9340 controller
 #define DISPLAY_SET_CURSOR_X 0x2A
 #define DISPLAY_SET_CURSOR_Y 0x2B
 #define DISPLAY_WRITE_PIXELS 0x2C
 
-// ILI9341 displays are able to update at any rate between 61Hz to up to 119Hz. Default at power on is 70Hz.
-#define ILI9341_FRAMERATE_61_HZ 0x1F
-#define ILI9341_FRAMERATE_63_HZ 0x1E
-#define ILI9341_FRAMERATE_65_HZ 0x1D
-#define ILI9341_FRAMERATE_68_HZ 0x1C
-#define ILI9341_FRAMERATE_70_HZ 0x1B
-#define ILI9341_FRAMERATE_73_HZ 0x1A
-#define ILI9341_FRAMERATE_76_HZ 0x19
-#define ILI9341_FRAMERATE_79_HZ 0x18
-#define ILI9341_FRAMERATE_83_HZ 0x17
-#define ILI9341_FRAMERATE_86_HZ 0x16
-#define ILI9341_FRAMERATE_90_HZ 0x15
-#define ILI9341_FRAMERATE_95_HZ 0x14
-#define ILI9341_FRAMERATE_100_HZ 0x13
-#define ILI9341_FRAMERATE_106_HZ 0x12
-#define ILI9341_FRAMERATE_112_HZ 0x11
-#define ILI9341_FRAMERATE_119_HZ 0x10
+// ILI9340 displays are able to update at any rate between 61Hz to up to 119Hz. Default at power on is 70Hz.
+#define ILI9340_FRAMERATE_61_HZ 0x1F
+#define ILI9340_FRAMERATE_63_HZ 0x1E
+#define ILI9340_FRAMERATE_65_HZ 0x1D
+#define ILI9340_FRAMERATE_68_HZ 0x1C
+#define ILI9340_FRAMERATE_70_HZ 0x1B
+#define ILI9340_FRAMERATE_73_HZ 0x1A
+#define ILI9340_FRAMERATE_76_HZ 0x19
+#define ILI9340_FRAMERATE_79_HZ 0x18
+#define ILI9340_FRAMERATE_83_HZ 0x17
+#define ILI9340_FRAMERATE_86_HZ 0x16
+#define ILI9340_FRAMERATE_90_HZ 0x15
+#define ILI9340_FRAMERATE_95_HZ 0x14
+#define ILI9340_FRAMERATE_100_HZ 0x13
+#define ILI9340_FRAMERATE_106_HZ 0x12
+#define ILI9340_FRAMERATE_112_HZ 0x11
+#define ILI9340_FRAMERATE_119_HZ 0x10
 
 // Visually estimating NES Super Mario Bros 3 "match mushroom, flower, star" arcade game, 119Hz gives visually
 // most pleasing result, so default to using that. You can also try other settings above. 119 Hz should give
 // lowest latency, perhaps 61 Hz might give least amount of tearing, although this can be quite subjective.
-#define ILI9341_UPDATE_FRAMERATE ILI9341_FRAMERATE_119_HZ
+#define ILI9340_UPDATE_FRAMERATE ILI9340_FRAMERATE_119_HZ
 
 #define DISPLAY_NATIVE_WIDTH 240
 #define DISPLAY_NATIVE_HEIGHT 320
 
-#ifdef ADAFRUIT_ILI9341_PITFT
-#include "pitft_28r_ili9341.h"
+#ifdef ADAFRUIT_ILI9340_PITFT
+#include "pitft_28r_ili9340.h"
 #elif defined(ADAFRUIT_HX8357D_PITFT)
 #include "pitft_35r_hx8357d.h"
 #elif defined(FREEPLAYTECH_WAVESHARE32B)
 #include "freeplaytech_waveshare32b.h"
 #endif
 
-#define InitSPIDisplay InitILI9341
+#define InitSPIDisplay InitILI9340
 
-void InitILI9341(void);
+void InitILI9340(void);
 
 #endif
