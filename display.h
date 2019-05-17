dce80e365c1cf8e907ad5f08e570af168d4585c3
Eugene Andruszczenko <eugene.andruszczenko@gmail.com>
2019-05-17 15:30:43 -0400

    Fri 17 May 2019 15:30:43 EDT - Added ILI9342 Driver

--- display.h
+++ display.h
@@ -7,6 +7,8 @@
 
 #if defined(ILI9341) || defined(ILI9340)
 #include "ili9341.h"
+#elif defined(ILI9342)
+#include "ili9342.h"
 #elif defined(ILI9486L)
 #include "ili9486l.h"
 #elif defined(ILI9486)
