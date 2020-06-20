07252c0afa9e5e569a069bfd0d6f3cc92adcd030
Marco Quezada <mquezada@mitre.org>
2020-06-20 18:04:24 -0400

    Update comments in header file.

--- ili9488.h
+++ ili9488.h
@@ -2,13 +2,11 @@
 
 #include "config.h"
 
-// On my Maithoga ILI9486L display (https://www.aliexpress.com/item/3-5-inch-8P-SPI-TFT-LCD-Color-Screen-Module-ILI9486-Drive-IC-320-480-RGB/32828284227.html), the following speed settings have been tested:
+// Tested with display model ER-TFT035-6 from BuyDisplay.com:
+// https://www.buydisplay.com/serial-spi-3-5-inch-tft-lcd-module-in-320x480-optl-touchscreen-ili9488
+// Using long dupont wires to connect to a Pi Zero, the clock divider was tested down to a value of 12.
 
-// core_freq=400: CDIV=8, results in 50.00MHz, works
-// core_freq=400: CDIV=6, would result in 66.666MHz, but this is too fast for the display, and produces corrupted output
-// core_freq=305: CDIV=6, would result in 50.833MHz, but this would work most of the time, producing occassional corrupted pixels
-
-// Data specific to the ILI9486L controller
+// Data specific to the ILI948X controllers
 #define DISPLAY_SET_CURSOR_X 0x2A
 #define DISPLAY_SET_CURSOR_Y 0x2B
 #define DISPLAY_WRITE_PIXELS 0x2C
@@ -16,10 +14,10 @@
 #define DISPLAY_NATIVE_WIDTH 320
 #define DISPLAY_NATIVE_HEIGHT 480
 
-// ILI9486L only supports 18 bits/pixel R6G6B6 format (padded to 3 bytes per pixel), and no 16-bits R5G6B5 mode.
+// 18 bits/pixel R6G6B6 format (padded to 3 bytes per pixel), and no 16-bits R5G6B5 mode.
 #define DISPLAY_COLOR_FORMAT_R6X2G6X2B6X2
 
-// ILI9486L does not behave well if one sends partial commands, but must finish each command or the command does not apply
+// ILI948X does not behave well if one sends partial commands, but must finish each command or the command does not apply
 #define MUST_SEND_FULL_CURSOR_WINDOW
 
 void InitILI9488(void);
